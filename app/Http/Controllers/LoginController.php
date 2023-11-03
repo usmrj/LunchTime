df2bcc844eb425033e521229754cad27f834b5d2
Michał Kujawski <mike@Michas-MacBook-Pro.local>
2023-11-03 15:46:20 +0100

    added users table and created LoginController

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+
+class LoginController extends Controller
+{
+    public function index() 
+    {
+        return view('login');
+    }
+
+    public function login(Request $request)
+    {
+        $data = $request->validate([
+            'login' => 'required',
+            'password' => 'required',
+        ]);
+        if (Auth::attempt(['email' => $data['login'], 'password' => $data['password']])) {
+            $request->session()->regenerate();
+            return redirect()->intended('/');
+        }
+
+        return back()->withErrors([
+            'login' => 'Login lub hasło nie sa poprawne.'
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/')->with('success', 'Pomyślnie wylogowano');
+    }
+}
