aa64d505b25a832a769dda3fc31c7985ffe33bea
MichalKujawski <m.kujawski2255@gmail.com>
2023-11-06 20:47:44 +0100

    added refunds migration

---
+++ database/migrations/2023_11_06_193404_create_refunds_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('refunds', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('student_id');
+            $table->foreign('student_id')
+            ->references('id')
+            ->on('students');
+            $table->integer('amount');
+            $table->date('ask_for_refund_date');
+            $table->date('refund_finished_date');
+            $table->tinyInteger('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('refunds');
+    }
+};
